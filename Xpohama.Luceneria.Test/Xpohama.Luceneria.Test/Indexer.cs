f2a1fb7923d496d7021a5aef22ac4be8a83cec42
xpohama <xpohama@gmail.com>
2013-02-05 22:23:35 +0100

    more of Lucene.Searchtests | Indexer.Searcher

--- Xpohama.Luceneria.Test/Xpohama.Luceneria.Test/Indexer.cs
+++ Xpohama.Luceneria.Test/Xpohama.Luceneria.Test/Indexer.cs
@@ -5,12 +5,17 @@ using Lucene.Net.Store;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Util;
 using Lucene.Net.Analysis;
+using Lucene.Net.Search;
 
 namespace Xpohama.Luceneria.Tests {
 
     public class Indexer {
         Version Version { get { return Version.LUCENE_29; } }
-       
+
+        public Indexer (Directory dir) {
+            this.Directory = dir;
+        }
+
         public Directory Directory { get; set; }
 
         IndexWriter _writer = null;
@@ -27,8 +32,41 @@ namespace Xpohama.Luceneria.Tests {
             }
         }
 
-        public Indexer (Directory dir) {
-            this.Directory = dir;
+        Analyzer _analyser = null;
+        public Analyzer Analyser {
+            get { return _analyser ?? (_analyser = new StandardAnalyzer(this.Version)); }
+            set { _analyser = value;}
+        }
+
+        bool refreshReader = false;
+        IndexReader _reader = null;
+        public IndexReader Reader {
+            get {
+                if (_reader == null) {
+                    if (_writer == null) {
+                        _reader = IndexReader.Open(this.Directory, true);
+                    } else {
+                        // get a near real time reader
+                        _reader = _writer.GetReader();
+                    }
+                } else if (refreshReader) {
+                    var newReader = _writer == null? _reader.Reopen():_writer.GetReader();
+                    if (_reader != newReader) {
+                        _reader.Close();
+                        _reader = newReader;
+                    }
+                    _searcher = null;
+                    refreshReader = false;
+                }
+                return _reader;
+            }
+        }
+
+        IndexSearcher _searcher = null;
+        public IndexSearcher Searcher {
+            get {
+                return _searcher ?? (_searcher = new IndexSearcher(this.Reader));
+            }
         }
 
         public void Close () {
@@ -36,20 +74,23 @@ namespace Xpohama.Luceneria.Tests {
                 _writer.Close();
                 _writer = null;
             }
+            if (_reader != null) {
+                _reader.Close();
+                _reader = null;
+            }
         }
 
+        public void Refresh() {
+            this.refreshReader = true;
+            _searcher = null;
+        }
+        public string DocumentContentField { get { return "contents"; } }
         public Document CreateDocument (System.Guid id, byte[] data) {
             var ex = new BodyTextExtractor();
             var doc = new Document();
-            doc.Add(new Field("contents", ex.ExtractText(data), Field.Store.NO, Field.Index.ANALYZED));
+            doc.Add(new Field(DocumentContentField, ex.ExtractText(data), Field.Store.NO, Field.Index.ANALYZED));
             doc.Add(new Field("ID", id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
             return doc;
-        }
-
-        Analyzer _analyser = null;
-        public Analyzer Analyser {
-            get { return _analyser ?? (_analyser = new StandardAnalyzer(this.Version)); }
-            set { _analyser = value;}
         }
     }
 }
--- Xpohama.Luceneria.Test/Xpohama.Luceneria.Test/LuceneTest.cs
+++ Xpohama.Luceneria.Test/Xpohama.Luceneria.Test/LuceneTest.cs
@@ -1,15 +1,22 @@
 using NUnit.Framework;
 using System.IO;
 using System;
+using Lucene.Net.Store;
+using Lucene.Net.Search;
+using Lucene.Net.Index;
+using System.Linq;
 
 namespace Xpohama.Luceneria.Tests {
     [TestFixture]
     public class LuceneTest {
 
         string TestDir = @"..\..\..\TestData\";
-        public Lucene.Net.Store.FSDirectory Directory { get {
-            return Lucene.Net.Store.FSDirectory.Open(new DirectoryInfo(TestDir+"Index"));
-        }}
+        public Lucene.Net.Store.Directory Directory {
+            get {
+                return new RAMDirectory(); 
+                    //FSDirectory.Open(new DirectoryInfo(TestDir + "Index"));
+            }
+        }
 
         [SetUp]
         public virtual void SetUp() {
@@ -17,19 +24,42 @@ namespace Xpohama.Luceneria.Tests {
         }
 
         Tuple<Guid,byte[]> ReadFile(string path) {
-            return Tuple.Create(Guid.NewGuid, File.ReadAllBytes(path));
+            return Tuple.Create(Guid.NewGuid(), File.ReadAllBytes(path));
         }
 
-       
-        public void IndexTest (string path) {
-            var indexer = new Indexer(this.Directory);
+
+        public void IndexTest (Indexer indexer, string path, string contains) {
+            contains = contains.ToLower();
+
             var file = ReadFile(path);
 
-            var doc = indexer.CreateDocument(file.Item1,file.Item2);
+            var doc = indexer.CreateDocument(file.Item1, file.Item2);
             indexer.Writer.AddDocument(doc);
             indexer.Writer.Commit();
+            indexer.Refresh();
 
-            //TODO: test if doc is indexed
+            var query = new TermQuery(new Term(indexer.DocumentContentField, contains));
+            var topDocs = indexer.Searcher.Search(query, 1000);
+            var docs = topDocs.scoreDocs
+                .Select(sd => indexer.Searcher.Doc(sd.doc))
+                .ToArray();
+
+            Assert.IsTrue(docs.Any(d => d.GetField("ID").StringValue() == file.Item1.ToString()));
+
+        }
+
+        [Test]
+        public void IndexerTest () {
+            var indexer = new Indexer(this.Directory);
+
+            IndexTest(indexer, TestDir + "Tika.rtf", "almonds");
+            IndexTest(indexer, TestDir + "Tika.pdf", "almonds");
+            IndexTest(indexer, TestDir + "Tika.docx", "interesting");
+            IndexTest(indexer, TestDir + "Tika.odt", "interesting");
+            IndexTest(indexer, TestDir + "Tika.pptx", "Presentation");
+            IndexTest(indexer, TestDir + "Tika.xlsx", "duke");
+
+            indexer.Close();
         }
 
         [Test]
